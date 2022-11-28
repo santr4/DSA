96f20073defdd20388dceac6b017bc7e06d59ca0
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2022-11-28 21:10:47 +0530

    codechef_array

--- codechef_array.cpp
+++ codechef_array.cpp
@@ -181,3 +181,44 @@ int main(){
     
     return 0;
 }
+
+// ** 4) quicksort
+#include<iostream>
+using namespace std;
+
+void swap(int arr[],int i,int j){
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int partition(int arr[],int l,int r){
+    int pivot = arr[r];
+    int i = l-1;
+    for(int j = 1;j<r;j++){
+        if(arr[j]<pivot){
+            i++;
+            swap(arr,i,j);
+        }
+    }
+    swap(arr,i+1,r);
+    return i+1;
+}
+
+void quicksort(int arr[],int l,int r){
+    if(l<r){
+        int pi = partition(arr,l,r);
+        quicksort(arr,l,pi-1);
+        quicksort(arr,pi+1,r);
+    }
+}
+
+int main(){
+    int arr[6] = {-12,2,30,13,1,5};
+    quicksort(arr,0,5);
+    cout<<"after applying the quicksort algorithm"<<endl;
+    for(int k = 0;k<6;k++){
+        cout<<arr[k]<<" ";
+    }
+    return 0;
+}
