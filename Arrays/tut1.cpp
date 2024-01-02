e024ceb99d4290506bac1bcdef3f93fed9308854
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-01-02 21:58:40 +0530

    remove duplicate elements in array using hashset

---
+++ Arrays/tut1.cpp
@@ -0,0 +1,34 @@
+// ** Remove duplicate elements in-place from sorted array.
+// ** brute force approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(int arr[], int n)
+{
+    set<int> set;
+    for (int i = 0; i < n; i++)
+    {
+        set.insert(arr[i]);
+    }
+    int k = set.size();
+    int j = 0;
+    for (int x : set)
+    {
+        arr[j++] = x;
+    }
+    return k;
+}
+
+int main()
+{
+    int arr[] = {1, 1, 2, 2, 2, 3, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = removeDuplicates(arr, n);
+    cout << "the array after removing duplicate elements" << endl;
+    for (int i = 0; i < k; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
